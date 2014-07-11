9d6ff947d82313235bf0c9bbaf714756b090f582
Martin Disch <martindisch@gmail.com>
2014-07-11 21:18:21 +0200

    Circular Reveal

--- app/src/main/java/com/martin/lolli/WidgetFragment.java
+++ app/src/main/java/com/martin/lolli/WidgetFragment.java
@@ -1,37 +1,40 @@
 package com.martin.lolli;
 
 
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 
 public class WidgetFragment extends Fragment {
 
-    private Button mButton;
+    private Button mButton, mVisibility;
+    private boolean mVisible = false;
     private RadioGroup mGroup;
     private RelativeLayout mCard;
     private int mElevation = 0;
+    private RelativeLayout mViews;
+
+    public WidgetFragment() {
+        // Required empty public constructor
+    }
 
     // TODO: Rename and change types and number of parameters
     public static WidgetFragment newInstance() {
         WidgetFragment fragment = new WidgetFragment();
         return fragment;
-    }
-    public WidgetFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -45,6 +48,60 @@ public class WidgetFragment extends Fragment {
                 mCard.setTranslationZ(mElevation);
             }
         });
+        mVisibility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mVisible) {
+                    // get the center for the clipping circle
+                    int cx = (mViews.getLeft() + mViews.getRight()) / 2;
+                    int cy = (mViews.getTop() + mViews.getBottom()) / 2;
+
+                    // get the initial radius for the clipping circle
+                    int initialRadius = mViews.getWidth();
+
+                    // create the animation (the final radius is zero)
+                    ValueAnimator anim =
+                            ViewAnimationUtils.createCircularReveal(mViews, cx, cy, initialRadius, 0);
+
+                    // make the view invisible when the animation is done
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mViews.setVisibility(View.INVISIBLE);
+                        }
+                    });
+
+                    // start the animation
+                    anim.start();
+                    mVisible = false;
+                } else {
+                    // get the center for the clipping circle
+                    int cx = (mViews.getLeft() + mViews.getRight()) / 2;
+                    int cy = (mViews.getTop() + mViews.getBottom()) / 2;
+
+                    // get the final radius for the clipping circle
+                    int finalRadius = mViews.getWidth();
+
+                    // create and start the animator for this view
+                    // (the start radius is zero)
+                    ValueAnimator anim =
+                            ViewAnimationUtils.createCircularReveal(mViews, cx, cy, 0, finalRadius);
+
+                    // make the view visible when the animation starts
+                    anim.addListener(new AnimatorListenerAdapter() {
+
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            super.onAnimationStart(animation);
+                            mViews.setVisibility(View.VISIBLE);
+                        }
+                    });
+                    anim.start();
+                    mVisible = true;
+                }
+            }
+        });
     }
 
     @Override
@@ -52,8 +109,10 @@ public class WidgetFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_widget, container, false);
         mButton = (Button) rootView.findViewById(R.id.bGo);
+        mVisibility = (Button) rootView.findViewById(R.id.bReveal);
         mGroup = (RadioGroup) rootView.findViewById(R.id.rgRadios);
         mCard = (RelativeLayout) rootView.findViewById(R.id.rlCard);
+        mViews = (RelativeLayout) rootView.findViewById(R.id.rlViews);
         return rootView;
     }
 
@@ -71,8 +130,7 @@ public class WidgetFragment extends Fragment {
                 SharedPreferences.Editor editor = sp.edit();
                 if (sp.getInt("Theme", R.style.Orange) == R.style.Orange) {
                     editor.putInt("Theme", R.style.Green);
-                }
-                else {
+                } else {
                     editor.putInt("Theme", R.style.Orange);
                 }
                 editor.commit();
